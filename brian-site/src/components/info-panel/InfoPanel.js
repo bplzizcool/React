bfc6955de4f9003ba73d9566b13b2ab2fed51a1c
Brian L <bplane92@gmail.com>
2024-03-12 18:20:56 -0400

    Drawer broke

--- brian-site/src/components/info-panel/InfoPanel.js
+++ brian-site/src/components/info-panel/InfoPanel.js
@@ -3,11 +3,18 @@ import Drawer from '@material-ui/core/Drawer';
 import InfoCard from '../info-card/InfoCard'
 import styles from './InfoPanel.module.css';
 
+const [open, setOpen] = React.useState(false);
+
+const toggleDrawer = (newOpen) => () => {
+  setOpen(newOpen);
+};
+
+
 class InfoPanel extends Component {
     render() {
         return (
             <div>
-                <Drawer className={styles.drawer} variant="permanent" classes={{paper: styles.drawerPaper}} anchor="left">
+                <Drawer onClick={toggleDrawer(true)} className={styles.drawer} open={open} variant="temporary" classes={{paper: styles.drawerPaper}} anchor="left">
                     <InfoCard></InfoCard>
                 </Drawer>
             </div>
